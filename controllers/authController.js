525023328f5a0ddb995739a8d51da77dd8c0272b
nishang.jx <nishang.jx@autonavi.com>
2021-07-28 16:14:53 +0800

    feat: 增加crud和登录注册

---
+++ controllers/authController.js
@@ -0,0 +1,55 @@
+const User = require("../models/userModel");
+const bcrypt = require('bcryptjs');
+exports.signup = async (req,res,next) =>{
+    try{
+        const {username,password} = req.body;
+        const hashpassword = await bcrypt.hash(password,12)
+        console.log("sign up",req.body)
+        const user = await User.create({username,password:hashpassword});
+        res.status(201).json({
+            status:'success',
+            data:{
+                user
+            }
+        })
+    }catch(e){
+        console.log(e)
+        res.status(400).json({
+            status:'fail'
+        })
+    }
+}
+
+exports.login = async (req,res,next)=>{
+    try{
+        const {username,password} = req.body;
+       
+        const user = await User.findOne({username});
+        console.log("login",req.body,user)
+        if (!user){
+            return res.status(400).json({
+                status:'fail',
+                message:'user not found'
+            })
+        }
+        const isCorrect = await bcrypt.compare(password,user.password);
+        console.log("login,isCorrect",isCorrect)
+        if (isCorrect) {
+            res.status(200).json({
+                status:'success'
+            })
+        }else{
+            res.status(400).json({
+                status:'fail',
+                message:'password is not correct'
+            })
+        }
+       
+    }catch(e){
+        console.log(e)
+        res.status(400).json({
+            status:'fail',
+            message:e
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const {MONGO_IP,MONGO_USER,MONGO_PASSWORD,MONGO_PORT}  = require('./config/config');
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 console.log('mongo config',MONGO_IP,MONGO_USER,MONGO_PASSWORD,MONGO_PORT)
-mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false})
+
+
+
+const connectWithRetry = ()=>{
+    mongoose.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false})
     .then(()=>console.log('链接mongodb成功'))
-    .catch((e)=>console.log(e))
+    .catch((e)=>{
+        console.log(e)
+        setTimeout(connectWithRetry,5000)
+    })
+}
+connectWithRetry();
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
+// const Cat = mongoose.model('Cat', { name: String });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-const Cat = mongoose.model('Cat', { name: String });
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('<h1>hello world,it is new world 11</h1>')
 })
+// 路由控制 localhost:3000/posts
+// app.use("/posts",postRouter);
+
+// localhost:3000/api/v1/posts
+app.use("/api/v1/posts",postRouter);
+app.use("/api/v1/users",userRouter);
+
 const port  = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`listening on port ${port}`)
